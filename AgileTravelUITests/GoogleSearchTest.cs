75cd367978b6d5f05efdf4c0b18c2ef99ccb24a2
izestrea <izestrea@gmail.com>
2016-07-22 11:14:31 +0300

    First commit that contain Google Search test

---
+++ AgileTravelUITests/GoogleSearchTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AgileTravelUITests
+{
+    [TestClass]
+    public class GoogleSearchTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("Hello Selenium WebDriver!");
+            element.Submit();
+            driver.Close();
+        }
+    }
+}
