63e1a42c2847291b0c77232d5b944f22f6dfe2bd
izestrea <izestrea@gmail.com>
2016-07-22 11:38:20 +0300

    Adding test method for other browsers

---
+++ AgileTravelUITests/GoogleSearchDifferentBrowserTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace AgileTravelUITests
+{
+    [TestClass]
+    public class GoogleSearchDifferentBrowserTest
+    {
+        [TestMethod]
+        public void TestInIE()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://testwisely.com/demo");
+            System.Threading.Thread.Sleep(1000);
+            driver.Quit();
+        }
+        [TestMethod]
+        public void TestInFirefox()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://testwisely.com/demo");
+            System.Threading.Thread.Sleep(1000);
+            driver.Quit();
+        }
+        [TestMethod]
+        public void TestInChrome()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://testwisely.com/demo");
+            System.Threading.Thread.Sleep(1000);
+            driver.Quit();
+        }
+
+    }
+}
